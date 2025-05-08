18d4cfa6db73e718f737a514cd5252a1265f8067
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-08 16:41:28 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function performs simplified long division.
+ */
+function calculate() {
+  const dividend = parseInt(document.getElementById("value-1").value);
+  const divisor = parseInt(document.getElementById("value-2").value);
+
+  // process
+  let remainder = dividend;
+  let quotient = 0;
+
+  while (remainder >= divisor) {
+    remainder -= divisor;
+    quotient++;
+  }
+
+  // output
+  document.getElementById("result").innerText = 
+    `Quotient: ${quotient}, Remainder: ${remainder}`;
+}
